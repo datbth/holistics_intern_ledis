6ea0908c09d567c5c59ae0c9d4fa0d78719b39c7
easd <scott.bui97@gmail.com>
2017-09-02 19:09:54 +0700

    add tests to git

--- cli/cli.js
+++ cli/cli.js
@@ -1,23 +1,28 @@
 var request = require('request');
+var assert = require('assert')
+var tests = require('./tests')
 const url = "http://128.199.166.9:3000/"
 
-function sendCommand(commandStr, completeHandler){
-    request.post({
-            headers: {
-                passwd:"BuiThanhDat"
-            },
-            url: url,
-            body: commandStr
-        }, function (error, response, body) {
-            if (error) {
-                console.log(error.message)
+function sendCommand(commandStr){
+    return new Promise (function(resolve, reject){
+        request.post({
+                headers: {
+                    passwd:"BuiThanhDat"
+                },
+                url: url,
+                body: commandStr
+            }, function (error, response, body) {
+                var result = ""
+                if (error) {
+                    result = error.message
+                }
+                else {
+                    result = body
+                }
+                resolve(body)
             }
-            else {
-                console.log(body); 
-            }
-            completeHandler()
-        }
-    );
+        );
+    })
 }
 
 var stdin = process.openStdin();
@@ -25,7 +30,29 @@ var stdin = process.openStdin();
 process.stdout.write(">Ledis: ")
 stdin.addListener("data", function (d) {
     var commandStr = d.toString().trim();
-    sendCommand(commandStr, function(){
-        process.stdout.write("\n>Ledis: ")
-    })
+    if (commandStr === 'test'){
+        var testPromise = Promise.resolve(null)
+        var i = 0;
+        function testCommand(){
+            var test = tests[i]
+            return sendCommand(test[0])
+                .then(result => {
+                    assert.strictEqual(result, test[1])
+                    i++
+                    console.log(i + " tests passed")
+                    if (i < tests.length) return testCommand()
+                }).catch(err => {
+                    console.error(err)
+                })
+        }
+        testCommand().then(()=>{
+            console.log("All passed")
+        })
+    }
+    else {
+        sendCommand(commandStr).then(result => {
+            console.log(result)
+            process.stdout.write("\n>Ledis: ")
+        })
+    }
 });
